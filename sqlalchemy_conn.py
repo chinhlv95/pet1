fc0db55b60a4ed32fc84e978bc85b721e03a7e72
hmit <thanh0014@gmail.com>
2019-09-19 00:40:00 +0700

    update

--- sqlalchemy_conn.py
+++ sqlalchemy_conn.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, flash, url_for, render_template, redirect
+from flask import Flask, request, flash, url_for, render_template, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 app.config['SECRET_KEY'] ='random string'
 
@@ -20,8 +22,10 @@ def __init__(self, name, city, addr, pin):
     self.pin = pin
 
 @app.route('/')
+@cross_origin(origin='*', headers=['Content- Type', 'Authorization'])
 def show_all():
-    return render_template('show_all.html', students = students.query.all())
+    return jsonify('asdasda')
+    # return render_template('show_all.html', students = students.query.all())
 
 @app.route('/new', methods = ['GET', 'POST'])
 def new():
