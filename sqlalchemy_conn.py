8d220c91e62b63dd582f6c90165ec2d8731cbf47
hmit <thanh0014@gmail.com>
2019-09-18 22:37:00 +0700

    update

--- sqlalchemy_conn.py
+++ sqlalchemy_conn.py
@@ -7,36 +7,33 @@ app.config['SECRET_KEY'] ='random string'
 db = SQLAlchemy(app)
 
 class students(db.Model):
-   id = db.Column('student_id', db.Integer, primary_key = True)
-   name = db.Column(db.String(100))
-   city = db.Column(db.String(50))
-   addr = db.Column(db.String(200))
-   pin = db.Column(db.String(10))
+    id = db.Column('student_id', db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    city = db.Column(db.String(100))
+    addr = db.Column(db.String(400))
+    pin = db.Column(db.String(100))
 
-def __init__(self, name, city, addr,pin):
-   self.name = name
-   self.city = city
-   self.addr = addr
-   self.pin = pin
-
+def __init__(self, name, city, addr, pin):
+    self.name = name
+    self.city = city
+    self.addr = addr
+    self.pin = pin
 
 @app.route('/')
 def show_all():
-    return render_template('show_all.html', students=students.query.all())
+    return render_template('show_all.html', students = students.query.all())
 
-
-@app.route('/new', methods=['GET', 'POST'])
+@app.route('/new', methods = ['GET', 'POST'])
 def new():
     if request.method == 'POST':
-        if not request.form['name'] or not request.form['city'] or not request.form['addr']:
+        if not request.form['name'] or not request.form['city'] or not request.form['addr'] or not request.form['pin']:
             flash('Please enter all the fields', 'error')
         else:
-            student = students(request.form['name'], request.form['city'],
-                               request.form['addr'], request.form['pin'])
-
+            form = request.form
+            student = students(name = form['name'], city = form['city'], addr = form['addr'], pin = form['pin'])
             db.session.add(student)
             db.session.commit()
-            flash('Record was successfully added')
+            flash('Record was added successfully')
             return redirect(url_for('show_all'))
     return render_template('new.html')
 
